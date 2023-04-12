46a0b61ce4da3571ff004e4da189da17fa626f22
Rabii Luena <itisrabii@gmail.com>
2023-04-12 12:48:46 +0300

    ref: moving types to a specific file

---
+++ src/types/index.d.ts
@@ -0,0 +1,32 @@
+import { Node } from "reactflow";
+
+export type ModelField = {
+  kind: "field";
+  id: string;
+  unique?: string;
+  name: string;
+  description?: string;
+  comment?: string;
+  dataType: DataTypes;
+  validations: Object[];
+};
+
+export type ModelData = Node & {
+  kind: "model";
+  id: string;
+  unique?: string;
+  name: string;
+  selected: Boolean;
+  description?: string;
+  comment?: string;
+  fields: ModelField[];
+};
+
+export type DataTypes =
+  | "Int"
+  | "Decimal"
+  | "String"
+  | "Text"
+  | "RichText"
+  | "Media"
+  | "Relation";
