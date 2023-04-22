fdf4501eeea597e06455a96b113a951dcba83ada
Rabii Luena <itisrabii@gmail.com>
2023-04-22 18:32:25 +0300

    feat: improving adding and deleting models through context menu

--- src/components/common/ContextMenu/ContextMenu.tsx
+++ src/components/common/ContextMenu/ContextMenu.tsx
@@ -7,6 +7,7 @@ import {
   KeyboardEvent,
   MouseEvent,
   forwardRef,
+  useState,
 } from "react";
 import { useReactFlow } from "reactflow";
 import { v4 as uuidV4 } from "uuid";
@@ -23,10 +24,59 @@ const ContextMenuComponent = ({ children }: Props) => {
   const flowInstance = useReactFlow();
   const inputRef = useRef<HTMLDivElement>(null);
   const clientPosRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [menuItems, setMenuItems] = useState<any[]>([]);
+  const modelIdRef = useRef();
 
-  function onContextMenu(evt: MouseEvent<HTMLSpanElement>) {
+  function onContextMenu(evt: MouseEvent<HTMLElement>) {
     clientPosRef.current.x = evt.clientX;
     clientPosRef.current.y = evt.clientY;
+    modelIdRef.current = evt.target
+      ?.closest("[data-id]")
+      ?.getAttribute("data-id");
+
+    flowInstance.setNodes((nodes) =>
+      nodes.filter((nodeItem) => nodeItem.id !== "newnodeinput")
+    );
+
+    if (evt.target?.classList.contains("react-flow__pane")) {
+      setMenuItems([
+        {
+          key: 12322,
+          label: "Model",
+          action: null,
+          shortcut: "⌘+N",
+          type: "label",
+        },
+        {
+          key: 23000,
+          label: "Add",
+          action: createNewModel,
+          shortcut: "⌘+N",
+          type: "item",
+        },
+      ]);
+    } else {
+      setMenuItems([
+        {
+          key: 12222,
+          label: "Model",
+          action: null,
+          shortcut: "⌘+N",
+          type: "label",
+        },
+        { key: 535555, label: "Delete", action: deleteCurrentModel },
+      ]);
+    }
+
+    // console.log(evt.target);
+  }
+
+  function deleteCurrentModel() {
+    const model = flowInstance.getNode(modelIdRef.current || "");
+
+    if (model) {
+      flowInstance.deleteElements({ nodes: [model] });
+    }
   }
 
   function createNewModel(evt: Event) {
@@ -104,17 +154,17 @@ const ContextMenuComponent = ({ children }: Props) => {
         {children}
       </ContextMenu.Trigger>
       <ContextMenu.Portal>
-        <CanvasMenu createNewModel={createNewModel} />
+        <CanvasMenu menuItems={menuItems} />
       </ContextMenu.Portal>
     </ContextMenu.Root>
   );
 };
 
 type MenuProps = {
-  createNewModel: (evt: Event) => void;
+  menuItems: { label: string; action: string }[];
 };
 
-const CanvasMenu = forwardRef<any, MenuProps>(({ createNewModel }, ref) => {
+const CanvasMenu = forwardRef<any, MenuProps>(({ menuItems }, ref) => {
   return (
     <ContextMenu.Content
       className="min-w-[220px] bg-white rounded-md overflow-hidden p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
@@ -122,15 +172,37 @@ const CanvasMenu = forwardRef<any, MenuProps>(({ createNewModel }, ref) => {
       alignOffset={5}
       ref={ref}
     >
-      <ContextMenu.Item
-        onSelect={createNewModel}
-        className="group text-[13px] leading-none text-violet-800 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet-600 data-[highlighted]:text-white"
-      >
-        Add model
-        {/* <div className="ml-auto pl-5 text-violet-800 group-data-[highlighted]:text-white group-data-[disabled]:text-slate-400">
-          ⌘+[
+      {menuItems.map((item: any) => {
+        if (item.type === "label") {
+          return (
+            <ContextMenu.Label
+              key={item.key}
+              className="pl-[25px] text-xs leading-[25px] text-slate-400"
+            >
+              {item.label}
+            </ContextMenu.Label>
+          );
+        }
+
+        return (
+          <ContextMenu.Item
+            key={item.key}
+            onSelect={item.action}
+            className="group text-[13px] leading-none text-violet-800 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-violet-400 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet-600 data-[highlighted]:text-white"
+          >
+            {item.label}
+
+            <span className="block ml-auto pl-5 text-violet-800 group-data-[highlighted]:text-white">
+              {item?.shortcut ? item.shortcut : null}
+            </span>
+
+            {/* <div className="ml-auto pl-5 text-violet-800 group-data-[highlighted]:text-white group-data-[disabled]:text-slate-400">
+
         </div> */}
-      </ContextMenu.Item>
+          </ContextMenu.Item>
+        );
+      })}
+
       {/* <ContextMenu.Item className="group text-[13px] leading-none text-violet-800 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet-600 data-[highlighted]:text-white">
         Add comment
       </ContextMenu.Item>
