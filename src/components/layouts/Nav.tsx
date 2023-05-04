a11ed9ea896e18527f8ef642c99740c29ffb5246
Rabii Luena <itisrabii@gmail.com>
2023-05-04 12:20:24 +0300

    improve: improving 404 and main navigation

--- src/components/layouts/Nav.tsx
+++ src/components/layouts/Nav.tsx
@@ -2,42 +2,37 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav>
-      <div className="w-full h-[60px] border-b border-b-slate-300 flex items-center">
-        <ul className="flex space-x-3 items-center justify-center mx-auto">
-          {/* <li>
+    <nav className="w-full fixed h-[60px] border-b border-b-slate-300 flex items-center">
+      <ul className="flex space-x-3 items-center justify-center mx-auto">
+        {/* <li>
               <Link className="text-indigo-700 text-sm font-bold" href="/">
                 Back
               </Link>
             </li> */}
-          <li>
-            <Link className="text-indigo-700 text-sm font-bold" href="/">
-              How it works?
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="text-indigo-700 text-sm font-bold"
-              href="/waitlist/try"
-            >
-              Try
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="text-indigo-700 text-sm font-bold"
-              href="/waitlist"
-            >
-              Join waitlist
-            </Link>
-          </li>
-          <li>
-            <Link className="text-indigo-700 text-sm font-bold" href="/roadmap">
-              Roadmap
-            </Link>
-          </li>
-        </ul>
-      </div>
+        <li>
+          <Link className="text-indigo-700 text-sm font-bold" href="/">
+            How it works?
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="text-indigo-700 text-sm font-bold"
+            href="/waitlist/try"
+          >
+            Try
+          </Link>
+        </li>
+        <li>
+          <Link className="text-indigo-700 text-sm font-bold" href="/waitlist">
+            Join waitlist
+          </Link>
+        </li>
+        <li>
+          <Link className="text-indigo-700 text-sm font-bold" href="/roadmap">
+            Roadmap
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
