93b6d9dc5a246b8922f37b91b2edcfbc396df82f
Rabii Luena <itisrabii@gmail.com>
2023-05-26 09:18:27 +0300

    ref: removing unwanted source codes

--- src/components/common/ModelNode/ModelNode.tsx
+++ src/components/common/ModelNode/ModelNode.tsx
@@ -1,10 +1,9 @@
 import { Position, Handle } from "reactflow";
-import { InformationCircleIcon } from "@heroicons/react/24/outline";
 import type { ModelField } from "@/types";
-import NodeField from "./NodeField";
 import { useModelsRelation } from "@/lib/client/hooks/useModelsRelation";
 import { useModelStore } from "@/lib/client/store/models";
 import useModelFields from "@/lib/client/hooks/useModelFields";
+import NodeField from "./NodeField";
 
 export type Props = {
   id: string;
@@ -24,24 +23,18 @@ const ModelNode = ({ id, name, modelId }: Props) => {
   const fieldsData = getModelFields(modelFieldIds);
 
   return (
-    <div className="bg-white rounded-md w-[230px] shadow-md divide-y divide-indigo-200 border-2 active:border-indigo-100 border-gray-200 model-node__wrapper">
+    <div className="bg-white rounded-md w-[230px] shadow-lg shadow-indigo-500/40 border active:border-2  active:border-indigo-100 border-gray-50 model-node__wrapper">
       {/* START: Node header */}
-      <div className="px-2 py-3 flex items-center justify-between">
-        <div className="pl-2">
+      <div className="px-4 py-3 flex items-center justify-between">
+        <div className="space-y-1">
           <span className="block leading-4 text-[16px]">{name}</span>
           <span className="block text-[10px] text-slate-500">{modelId}</span>
         </div>
-        <button
-          className="text-indigo-500 active:text-indigo-300"
-          // onClick={() => console.log("Something")}
-        >
-          <InformationCircleIcon strokeWidth={2} className="text-lg w-6 h-6" />
-        </button>
       </div>
       {/* END: Node header */}
 
       {/* START: Node fields */}
-      <div className="px-2 space-y-2 py-2 relative">
+      <div className="px-4 space-y-2 py-2 relative">
         {fieldsData?.map((field: ModelField) => (
           <NodeField
             key={field.id}
