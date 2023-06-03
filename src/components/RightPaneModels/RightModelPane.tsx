daa313280fa16e9f4ca3bf0a49b6a2d5aed95fde
Rabii Luena <itisrabii@gmail.com>
2023-06-03 11:06:49 +0300

    ref: moving onSelectingModel to useModels hook

--- src/components/RightPaneModels/RightModelPane.tsx
+++ src/components/RightPaneModels/RightModelPane.tsx
@@ -1,4 +1,4 @@
-import { Node, useOnSelectionChange, useReactFlow } from "reactflow";
+import { useOnSelectionChange, useReactFlow } from "reactflow";
 import isEmpty from "lodash/isEmpty";
 import { useModelStore } from "@/lib/client/store/models";
 import useModels from "@/lib/client/hooks/useModels";
@@ -9,9 +9,9 @@ type Props = {
 };
 
 const RightModelPane = ({ showMultipleModels }: Props) => {
-  const { setNodes, setCenter, getZoom, deleteElements } = useReactFlow();
+  const { deleteElements } = useReactFlow();
   const setActiveModel = useModelStore((state) => state.setActiveModel);
-  const { getModelsData, selectedActiveModel } = useModels();
+  const { getModelsData, selectedActiveModel, onSelectingModel } = useModels();
 
   const modelsData = getModelsData();
 
@@ -21,38 +21,6 @@ const RightModelPane = ({ showMultipleModels }: Props) => {
   useOnSelectionChange({
     onChange: ({ nodes }) => setActiveModel(nodes[0]?.id),
   });
-
-  /**
-   *
-   * @param modelId
-   */
-  function onSelectingModel(modelId: string) {
-    let currSelectingNode;
-
-    setNodes((nodes) => {
-      return nodes.map((item) => {
-        if (modelId === item.id && !item.selected) {
-          item.selected = true;
-          currSelectingNode = item as Node;
-        } else {
-          item.selected = false;
-        }
-
-        return item;
-      });
-    });
-
-    if (currSelectingNode) {
-      setCenter(
-        Number(currSelectingNode!.position.x + 200),
-        currSelectingNode!.position.y,
-        {
-          duration: 500,
-          zoom: getZoom(),
-        }
-      );
-    }
-  }
 
   /**
    * Removing model node from canvas, hence the model will be deleted from the store automatically,
